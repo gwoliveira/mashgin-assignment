dfe056acaa43e6bc8ad50c92d9cc3e2ffc568526
Guilherme Willian de Oliveira <gwoliveira@gmail.com>
2025-10-04 17:42:26 -0300

    feat: Initial project setup with FastAPI, Docker, and Alembic

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,14 @@
-from typing import Union
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
 
-from fastapi import FastAPI
+from database import SessionLocal, engine
+
+
 
 app = FastAPI()
+
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
