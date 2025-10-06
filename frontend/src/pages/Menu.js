43d34d3e8123ee655e90d35a2e2f56ee14340d3c
Guilherme Willian de Oliveira <gwoliveira@gmail.com>
2025-10-06 18:55:09 -0300

    feat: Refactor Menu component to use MenuGrid for item display

--- frontend/src/pages/Menu.js
+++ frontend/src/pages/Menu.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { getCategories, getItems } from '../api';
-import Item from '../components/Item';
 import Category from '../components/Category';
 import Cart from '../components/Cart';
-import { Grid, Box } from '@mui/material';
+import MenuGrid from '../components/MenuGrid';
+import { Box } from '@mui/material';
 
 const Menu = () => {
   const [categories, setCategories] = useState([]);
@@ -51,13 +51,7 @@ const Menu = () => {
           handleCategoryClick={handleCategoryClick}
           sx={{ mt: 4 }}
         />
-        <Grid container spacing={2}>
-          {filteredItems.map((item) => (
-            <Grid item xs={12} sm={6} md={4} key={item.id}>
-              <Item item={item} />
-            </Grid>
-          ))}
-        </Grid>
+        <MenuGrid items={filteredItems} />
       </Box>
       <Box sx={{ width: '300px', ml: 4 }}>
         <Cart />
