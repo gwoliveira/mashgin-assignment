7b226695f7423d407b146f921ad9e5a122e4c6f4
Guilherme Willian de Oliveira <gwoliveira@gmail.com>
2025-10-06 22:30:01 -0300

    backgroud task to simulate order completed

--- backend/app/routers/orders.py
+++ backend/app/routers/orders.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, BackgroundTasks
 from sqlalchemy.orm import Session
 from app.dependencies import get_db
 from app.schemas.order import OrderCreate, Order
@@ -7,9 +7,9 @@ from app.services.order_service import OrderService
 router = APIRouter()
 
 @router.post("/", response_model=Order)
-def create_order(order: OrderCreate, db: Session = Depends(get_db)):
+def create_order(order: OrderCreate, background_tasks: BackgroundTasks, db: Session = Depends(get_db)):
     service = OrderService(db)
-    return service.create_order(order)
+    return service.create_order(order, background_tasks)
 
 @router.get("/{order_id}", response_model=Order)
 def get_order_details(order_id: int, db: Session = Depends(get_db)):
